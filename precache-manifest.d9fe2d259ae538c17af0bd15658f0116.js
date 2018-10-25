ed35f762b23a88e69741df194dff7ba79501ccf5
Andrew Bonifacio <contact@andrewbdesign.com>
2018-10-25 11:40:13 +1100

    Updates

--- precache-manifest.d9fe2d259ae538c17af0bd15658f0116.js
+++ precache-manifest.d9fe2d259ae538c17af0bd15658f0116.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/snakk-media-audiences/static/js/runtime~main.4ff67542.js"
   },
   {
-    "revision": "0e7d3e895e4314808335",
-    "url": "/snakk-media-audiences/static/js/main.0e7d3e89.chunk.js"
+    "revision": "7ad1a46c15696407c569",
+    "url": "/snakk-media-audiences/static/js/main.7ad1a46c.chunk.js"
   },
   {
     "revision": "a01c4770fbf3e24b1f8e",
     "url": "/snakk-media-audiences/static/js/1.a01c4770.chunk.js"
   },
   {
-    "revision": "0e7d3e895e4314808335",
-    "url": "/snakk-media-audiences/static/css/main.fde3ea30.chunk.css"
+    "revision": "7ad1a46c15696407c569",
+    "url": "/snakk-media-audiences/static/css/main.299d23ae.chunk.css"
   },
   {
     "revision": "a01c4770fbf3e24b1f8e",
     "url": "/snakk-media-audiences/static/css/1.c569e928.chunk.css"
   },
   {
-    "revision": "60995eb9cbbe4af8bfa12b5b0242d487",
+    "revision": "c5169d719a5d413ea028f09e503a342d",
     "url": "/snakk-media-audiences/index.html"
   }
 ];
